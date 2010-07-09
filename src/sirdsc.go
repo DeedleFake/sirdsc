a3cef3e39a1ddb3b94b12d77fc74ffba2c6fb184
DeedleFake <YissZev@BeckForce.com>
2010-07-08 22:54:00 -0400

    Began working on parsing the arguments.

--- src/sirdsc.go
+++ src/sirdsc.go
@@ -1,9 +1,33 @@
 package main
 
 import(
+	"os"
 	"fmt"
+	"strings"
+	"strconv"
 )
 
+func usage() {
+	fmt.Printf("\033[0;1mUsage:\033[m\n")
+	fmt.Printf("\t%s <in> <out> <part-size>\n", os.Args[0])
+}
+
 func main() {
-	fmt.Printf("Testing, testing, 1, 2, 3.\n")
+	if len(os.Args) != 4 {
+		usage()
+		os.Exit(1)
+	}
+
+	inN := os.Args[1]
+	outN := os.Args[2]
+	partSize, _ := strconv.Atoi(os.Args[3])
+
+	switch (strings.ToLower(inN[len(inN)-3:])) {
+		case "jpg":
+			fmt.Printf("Jpeg.\n")
+		case "png":
+			fmt.Printf("PNG.\n")
+	}
+
+	fmt.Printf(outN, partSize)
 }
