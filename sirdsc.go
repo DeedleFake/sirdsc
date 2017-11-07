5a466aaeaee56460da6d9b472ba2a36dcef9f13a
DeedleFake <deedlefake@users.noreply.github.com>
2017-11-07 13:21:47 -0500

    When generating, only set the depth pixel if it's different from the original.

--- sirdsc.go
+++ sirdsc.go
@@ -10,7 +10,8 @@ import (
 // to out, using the pattern pat. partSize specifies the width of a
 // single section of the generated stereogram. If partSize is less
 // than or equal to zero, the width of pat is used. 100 is recommended
-// as a good default.
+// as a good default, but this heavily depends on the physical size of
+// the screen that the stereogram will be displayed on.
 func Generate(out draw.Image, dm DepthMap, pat image.Image, partSize int) {
 	if partSize <= 0 {
 		partSize = pat.Bounds().Dx()
@@ -20,13 +21,15 @@ func Generate(out draw.Image, dm DepthMap, pat image.Image, partSize int) {
 		Image: pat,
 	}
 
+	b := out.Bounds()
+
 	var wg sync.WaitGroup
-	wg.Add(out.Bounds().Dy())
-	for y := 0; y < out.Bounds().Dy(); y++ {
+	wg.Add(b.Dy())
+	for y := b.Min.Y; y < b.Max.Y; y++ {
 		go func(y int) {
 			defer wg.Done()
 
-			for x := 0; x < out.Bounds().Dx(); x++ {
+			for x := b.Min.X; x < b.Max.X; x++ {
 				depth := dm.At(x-partSize, y)
 
 				src := pat
@@ -34,10 +37,10 @@ func Generate(out draw.Image, dm DepthMap, pat image.Image, partSize int) {
 					src = out
 				}
 
-				if x-depth >= 0 {
-					c := src.At(x-partSize, y)
+				c := src.At(x-partSize, y)
+				out.Set(x, y, c)
 
-					out.Set(x, y, c)
+				if depth != 0 {
 					out.Set(x-depth, y, c)
 				}
 			}
