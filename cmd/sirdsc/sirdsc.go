18483a23a90bcab719a4f8bed45738d66355b3cc
DeedleFake <deedlefake@users.noreply.github.com>
2017-11-09 12:59:37 -0500

    Allow reading and writing from and to stdin and stdout in the command.

--- cmd/sirdsc/sirdsc.go
+++ cmd/sirdsc/sirdsc.go
@@ -7,6 +7,7 @@ import (
 	_ "image/gif"
 	_ "image/jpeg"
 	"image/png"
+	"io"
 	"os"
 	"time"
 
@@ -18,11 +19,14 @@ import (
 )
 
 func loadImage(file string) (image.Image, error) {
-	f, err := os.Open(file)
-	if err != nil {
-		return nil, err
+	f := io.Reader(os.Stdin)
+	if (file != "") && (file != "-") {
+		f, err := os.Open(file)
+		if err != nil {
+			return nil, err
+		}
+		defer f.Close()
 	}
-	defer f.Close()
 
 	img, _, err := image.Decode(f)
 	return img, err
@@ -30,18 +34,21 @@ func loadImage(file string) (image.Image, error) {
 
 // TODO: Encode different types based on the extension.
 func saveImage(file string, img image.Image) error {
-	f, err := os.Create(file)
-	if err != nil {
-		return err
+	f := io.Writer(os.Stdout)
+	if (file != "") && (file != "-") {
+		f, err := os.Create(file)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
 	}
-	defer f.Close()
 
 	return png.Encode(f, img)
 }
 
 func main() {
 	flag.Usage = func() {
-		fmt.Fprintf(os.Stderr, "Usage: %v [options] <src>\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "Usage: %v [options] [src]\n", os.Args[0])
 		fmt.Fprintf(os.Stderr, "Options:\n")
 		flag.PrintDefaults()
 	}
@@ -52,14 +59,18 @@ func main() {
 	seed := flag.Int64("seed", int64(time.Since(time.Time{})), "Color generation seed")
 	sym := flag.Bool("sym", false, "Use symmetric generation")
 	patFile := flag.String("pat", "", "If not empty, use the specified file as the pattern instead of randomizing")
-	outFile := flag.String("o", "sirds.png", "Output file")
+	outFile := flag.String("o", "", "Output file")
 	flag.Parse()
 
-	if flag.NArg() != 1 {
+	var inFile string
+	switch flag.NArg() {
+	case 0:
+	case 1:
+		inFile = flag.Arg(0)
+	default:
 		flag.Usage()
 		os.Exit(2)
 	}
-	inFile := flag.Arg(0)
 
 	inImg, err := loadImage(inFile)
 	if err != nil {
