1e0435af1c73f6324f26ebb117963307a60f0cff
DeedleFake <deedlefake@users.noreply.github.com>
2017-11-11 23:39:26 -0500

    Use Pixel for the game example, instead of partial SDL bindings.

--- examples/game/game.go
+++ examples/game/game.go
@@ -8,7 +8,8 @@ import (
 	"time"
 
 	"github.com/DeedleFake/sirdsc"
-	"github.com/DeedleFake/sirdsc/examples/game/internal/sdl"
+	"github.com/faiface/pixel"
+	"github.com/faiface/pixel/pixelgl"
 )
 
 const (
@@ -31,9 +32,9 @@ const (
 // objects as depths, rather than colors.
 type DepthMap struct {
 	Depth int
-	Rect  image.Rectangle
+	Rect  pixel.Rect
 
-	Obstacle image.Rectangle
+	Obstacle pixel.Rect
 }
 
 func (dm DepthMap) Bounds() image.Rectangle { // nolint
@@ -41,138 +42,123 @@ func (dm DepthMap) Bounds() image.Rectangle { // nolint
 }
 
 func (dm DepthMap) At(x, y int) int { // nolint
-	p := image.Pt(x, y)
+	v := pixel.V(float64(x), float64(y))
 
-	if p.In(dm.Rect) && p.In(dm.Obstacle) {
+	if dm.Rect.Contains(v) && dm.Obstacle.Contains(v) {
 		return int(math.Max(float64(dm.Depth), 10))
 	}
-	if p.In(dm.Rect) {
+	if dm.Rect.Contains(v) {
 		return dm.Depth
 	}
-	if p.In(dm.Obstacle) {
+	if dm.Obstacle.Contains(v) {
 		return 10
 	}
 
 	return 0
 }
 
-func main() {
-	err := sdl.Init()
-	if err != nil {
-		log.Fatalf("Failed to initialize SDL: %v", err)
-	}
-	defer sdl.Quit()
-
-	win, ren, err := sdl.CreateWindowAndRenderer(ScreenWidth+PartSize, ScreenHeight, 0)
-	if err != nil {
-		log.Fatalf("Failed to create window: %v", err)
-	}
-	defer ren.Destroy()
-	defer win.Destroy()
-
-	out, err := ren.CreateTexture(
-		sdl.PIXELFORMAT_ABGR8888,
-		sdl.TEXTUREACCESS_STREAMING,
-		ScreenWidth+PartSize, ScreenHeight,
+func toImageRect(r pixel.Rect) image.Rectangle {
+	return image.Rect(
+		int(r.Min.X),
+		int(r.Min.Y),
+		int(r.Max.X),
+		int(r.Max.Y),
 	)
-	if err != nil {
-		log.Fatalf("Failed to create texture: %v", err)
-	}
-	defer out.Destroy()
+}
 
-	keys := make(map[uint32]bool)
-	keyDown := func(c uint32) bool {
-		d, _ := keys[c]
-		return d
-	}
+func main() {
+	pixelgl.Run(func() {
+		win, err := pixelgl.NewWindow(pixelgl.WindowConfig{
+			Title:  "SIRDS",
+			Bounds: pixel.R(0, 0, ScreenWidth+PartSize, ScreenHeight),
+			VSync:  true,
+		})
+		if err != nil {
+			log.Fatalf("Failed to create window: %v", err)
+		}
+		defer win.Destroy()
+		win.SetMatrix(pixel.IM.Moved(win.Bounds().Center()))
 
-	dm := DepthMap{
-		Depth: 10,
-		Rect:  image.Rect(100, 100, 200, 200),
+		out := image.NewNRGBA(toImageRect(win.Bounds()))
 
-		Obstacle: image.Rect(
-			ScreenWidth/2-35,
-			ScreenHeight/2-35,
-			ScreenWidth/2+35,
-			ScreenHeight/2+35,
-		),
-	}
+		dm := DepthMap{
+			Depth: 10,
+			Rect:  pixel.R(100, 100, 200, 200),
 
-	tick := time.NewTicker(time.Second / 60)
-	defer tick.Stop()
-
-	var frames uint
-	last := struct {
-		ts    time.Time
-		frame uint
-	}{
-		ts: time.Now(),
-	}
-
-	for start := range tick.C {
-		// TODO: Scale movement speed.
-		fps := float64(frames-last.frame) / start.Sub(last.ts).Seconds()
-		if start.Sub(last.ts) > FPSDelay {
-			fmt.Printf("FPS: %v\n", fps)
-
-			last.ts = start
-			last.frame = frames
+			Obstacle: pixel.R(
+				ScreenWidth/2-35,
+				ScreenHeight/2-35,
+				ScreenWidth/2+35,
+				ScreenHeight/2+35,
+			),
 		}
 
-		for {
-			ev := sdl.PollEvent()
-			if ev == nil {
-				break
+		seed := time.Now().UnixNano()
+
+		tick := time.NewTicker(time.Second / 60)
+		defer tick.Stop()
+
+		var frames uint
+		last := struct {
+			ts    time.Time
+			frame uint
+		}{
+			ts: time.Now(),
+		}
+
+		for !win.Closed() {
+			// TODO: Scale movement speed.
+			start := time.Now()
+			fps := float64(frames-last.frame) / start.Sub(last.ts).Seconds()
+			if start.Sub(last.ts) > FPSDelay {
+				fmt.Printf("FPS: %v\n", fps)
+
+				last.ts = start
+				last.frame = frames
 			}
 
-			switch ev := ev.(type) {
-			case sdl.QuitEvent:
-				return
-
-			case sdl.KeyUpEvent:
-				keys[ev.Keysym().Sym()] = false
-			case sdl.KeyDownEvent:
-				keys[ev.Keysym().Sym()] = true
+			if win.Pressed(pixelgl.KeyUp) {
+				dm.Rect.Min.Y -= 10
+				dm.Rect.Max.Y -= 10
 			}
-		}
+			if win.Pressed(pixelgl.KeyDown) {
+				dm.Rect.Min.Y += 10
+				dm.Rect.Max.Y += 10
+			}
+			if win.Pressed(pixelgl.KeyLeft) {
+				dm.Rect.Min.X -= 10
+				dm.Rect.Max.X -= 10
+			}
+			if win.Pressed(pixelgl.KeyRight) {
+				dm.Rect.Min.X += 10
+				dm.Rect.Max.X += 10
+			}
 
-		if keyDown(sdl.K_UP) {
-			dm.Rect.Min.Y -= 10
-			dm.Rect.Max.Y -= 10
-		}
-		if keyDown(sdl.K_DOWN) {
-			dm.Rect.Min.Y += 10
-			dm.Rect.Max.Y += 10
-		}
-		if keyDown(sdl.K_LEFT) {
-			dm.Rect.Min.X -= 10
-			dm.Rect.Max.X -= 10
-		}
-		if keyDown(sdl.K_RIGHT) {
-			dm.Rect.Min.X += 10
-			dm.Rect.Max.X += 10
-		}
+			if win.Pressed(pixelgl.KeyW) {
+				dm.Depth--
+			}
+			if win.Pressed(pixelgl.KeyS) {
+				dm.Depth++
+			}
+			if dm.Depth < 5 {
+				dm.Depth = 5
+			}
+			if dm.Depth > 20 {
+				dm.Depth = 20
+			}
 
-		if keyDown(sdl.K_w) {
-			dm.Depth--
-		}
-		if keyDown(sdl.K_s) {
-			dm.Depth++
-		}
-		if dm.Depth < 5 {
-			dm.Depth = 5
-		}
-		if dm.Depth > 20 {
-			dm.Depth = 20
-		}
+			if s := time.Now().UnixNano(); s-seed > int64(time.Second/30) {
+				seed = s
+			}
 
-		img := out.Image()
-		sirdsc.Generate(img, dm, sirdsc.RandImage(frames), PartSize)
-		img.Close()
+			sirdsc.Generate(out, dm, sirdsc.RandImage(seed), PartSize)
+			pic := pixel.PictureDataFromImage(out)
+			s := pixel.NewSprite(pic, pic.Bounds())
+			s.Draw(win, pixel.IM)
 
-		ren.Copy(out, image.ZR, image.ZR)
-		ren.Present()
+			win.Update()
 
-		frames++
-	}
+			frames++
+		}
+	})
 }
