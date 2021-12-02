d80d32ae79918810caf4c9aa55567304017ab618
DeedleFake <deedlefake@users.noreply.github.com>
2021-12-02 00:58:18 -0500

    spcg: allow all functions to be inlined

--- spcg/spcg.go
+++ spcg/spcg.go
@@ -27,23 +27,24 @@ const (
 // Next returns the next random number in the PCG sequence, along with
 // the high and low bits for getting the number after the one
 // returned.
-func Next(high, low uint64) (r, rhigh, rlow uint64) {
-	pcg := pcg{high, low}
-	pcg.multiply()
-	pcg.add()
-	return bits.RotateLeft64(pcg.high^pcg.low, -int(pcg.high>>58)), pcg.high, pcg.low
+func Next(high, low uint64) (n, nhigh, nlow uint64) {
+	high, low = multiply(high, low)
+	high, low = add(high, low)
+	return bits.RotateLeft64(high^low, -int(high>>58)), high, low
 }
 
-func (pcg *pcg) add() {
+func add(high, low uint64) (uint64, uint64) {
 	var carry uint64
-	pcg.low, carry = bits.Add64(pcg.low, incLow, 0)
-	pcg.high, _ = bits.Add64(pcg.high, incHigh, carry)
+	low, carry = bits.Add64(low, incLow, 0)
+	high, _ = bits.Add64(high, incHigh, carry)
+	return high, low
 }
 
-func (pcg *pcg) multiply() {
-	hi, lo := bits.Mul64(pcg.low, mulLow)
-	hi += pcg.high * mulLow
-	hi += pcg.low * mulHigh
-	pcg.low = lo
-	pcg.high = hi
+func multiply(high, low uint64) (uint64, uint64) {
+	hi, lo := bits.Mul64(low, mulLow)
+	hi += high * mulLow
+	hi += low * mulHigh
+	low = lo
+	high = hi
+	return high, low
 }
