34d75d295062ce8225e9b8b94efe868341fdcc96
DeedleFake <deedlefake@users.noreply.github.com>
2026-02-16 20:26:03 -0500

    examples/web: implement the display

--- examples/web/src/Display.tsx
+++ examples/web/src/Display.tsx
@@ -1,3 +1,5 @@
+import { useMemo } from "react";
+
 export type DisplayProps = {
   params: Params;
 };
@@ -14,7 +16,19 @@ export type Params = {
 };
 
 export function Display({ params }: DisplayProps) {
-  return <div>Not implemented.</div>;
+  const query = useMemo(
+    () =>
+      Object.entries(params).reduce((query, [name, val]) => {
+        query.set(name, `${val}`);
+        return query;
+      }, new URLSearchParams()),
+    [params],
+  );
+  const src = useMemo(() => `/generate?${query}`, [query]);
+
+  return src ? (
+    <img className="flex-1 m-4" alt="Display" src={src} />
+  ) : null;
 }
 
 export default Display;
